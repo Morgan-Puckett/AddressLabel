f84eca0c9a95cef91f09d72967b846f0ed110c9e
Morgan Puckett <morganpuckett02@gmail.com>
2022-03-23 15:22:58 -0600

    Documented

--- AddressLabelForm.vb
+++ AddressLabelForm.vb
@@ -2,15 +2,14 @@
 'RCET0265
 'Spring 2022
 'Address Label
-' 
-
-
+'https://github.com/Morgan-Puckett/AddressLabel.git
 
 Public Class AddressLabelForm
+    'Varibles to allow cleaner code
     Dim finalLabel As String
     Dim areaSpec As String
 
-
+    'Displays given information into the display label
     Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
         finalLabel = FirstNameTextBox.Text + " " + LastNameTextBox.Text
         finalLabel = finalLabel + vbCrLf + StreetAddressTextBox.Text
@@ -20,6 +19,7 @@ Public Class AddressLabelForm
         DisplayLabel.Text = finalLabel
     End Sub
 
+    'Clears current Information
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         finalLabel = " "
         DisplayLabel.Text = finalLabel
@@ -31,6 +31,7 @@ Public Class AddressLabelForm
         ZipTextBox.Text = ""
     End Sub
 
+    'Closes the form 
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
